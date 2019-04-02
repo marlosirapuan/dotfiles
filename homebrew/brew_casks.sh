088f01c1f2716e5ac81279d6b57065b7edcc263c
Marlos <marlos.irapuan@gmail.com>
2019-04-02 18:30:38 -0300

    Create brew_casks.sh

---
+++ homebrew/brew_casks.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Install some apps.
+
+# Utils
+brew cask install spectacle
+brew cask install 1password
+
+# Dev
+brew cask install iterm2
+brew cask install visual-studio-code
+brew cask install imagealpha
+brew cask install imageoptim
+
+# Extras
+brew cask install vlc
+brew cask install spotify
+brew cask install transmission
+brew cask install gyazo
+brew cask install licecap
+brew cask install fastscripts
+
+# Fonts (https://github.com/Homebrew/homebrew-cask-fonts/tree/master/Casks)
+brew tap caskroom/fonts 
+brew cask install font-fira-code
