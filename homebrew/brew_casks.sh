a185264024f8f5816850734af934968dc63b0d28
Marlos <marlos.irapuan@gmail.com>
2019-07-25 11:11:44 -0300

    Remove fastscripts and add font hack nerd

--- homebrew/brew_casks.sh
+++ homebrew/brew_casks.sh
@@ -4,14 +4,12 @@
 
 # Utils
 brew cask install spectacle
-# brew cask install 1password
 
 # Dev
 brew cask install iterm2
 brew cask install visual-studio-code
 brew cask install imagealpha
 brew cask install imageoptim
-brew cask install fastscripts
 
 # Extras
 brew cask install vlc
@@ -19,9 +17,8 @@ brew cask install spotify
 brew cask install transmission
 brew cask install gyazo
 brew cask install licecap
-brew cask install fastscripts
 
 # Fonts (https://github.com/Homebrew/homebrew-cask-fonts/tree/master/Casks)
 brew tap caskroom/fonts 
-brew cask install font-hack
-brew cask install font-fira-code
+brew cask install font-hack-nerd-font-mono
+brew cask install font-hack-nerd-font
